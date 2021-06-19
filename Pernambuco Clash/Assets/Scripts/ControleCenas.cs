febab210ecdeef01d942f254688f3832146836bc
Alan Marinho <kgometalenator@gmail.com>
2021-06-19 02:01:58 -0300

    Menu interativo

--- Pernambuco Clash/Assets/Scripts/ControleCenas.cs
+++ Pernambuco Clash/Assets/Scripts/ControleCenas.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControleCenas : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static ControleCenas instance;
+    public GameObject opcoes;
+    private void Awake()
+    {
+        instance = this;
+    }
+ 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void NovoJogoScene()
+    {
+        //SceneManager.LoadScene ("Novo Jogo");
+    }
+
+    public void SairDoJogo(){
+        Application.Quit();
+    }
+
+    public void MostrarOpcoes(){
+        opcoes.SetActive(true);
+    }
+
+    public void VoltarMenuPrincipal(){
+        opcoes.SetActive(false);
     }
 }
