3e1372d213888aeb37d8ce70d03a6d72d8037e49
teteretu <teteretu@yahoo.com>
2021-06-27 18:42:08 -0300

    implement pause function

--- Pernambuco Clash/Assets/Scripts/ControleCenas.cs
+++ Pernambuco Clash/Assets/Scripts/ControleCenas.cs
@@ -7,21 +7,38 @@ public class ControleCenas : MonoBehaviour
 {
     public static ControleCenas instance;
     public GameObject opcoes;
-    
+    public bool paused = false;
+
     private void Awake()
     {
         if (instance == null)
             instance = this;
     }
- 
+
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        
+
+    }
+
+    public void PauseAndResumeGame()
+    {
+        if (paused)
+        {
+            paused = false;
+            Time.timeScale = 1;
+            opcoes.SetActive(false);
+        }
+        else
+        {
+            opcoes.SetActive(true);
+            paused = true;
+            Time.timeScale = 0;
+        }
     }
 
     public void LoadScene(string scene)
@@ -31,22 +48,26 @@ public class ControleCenas : MonoBehaviour
 
     public void NovoJogoScene()
     {
-        SceneManager.LoadScene ("Fase1");
+        SceneManager.LoadScene("Fase1");
     }
 
-    public void SairDoJogo(){
+    public void SairDoJogo()
+    {
         Application.Quit();
     }
 
-    public void MostrarOpcoes(){
+    public void MostrarOpcoes()
+    {
         opcoes.SetActive(true);
     }
 
-    public void VoltarMenuPrincipal(){
+    public void VoltarMenuPrincipal()
+    {
         opcoes.SetActive(false);
     }
 
-    public void GameOverScene(){
+    public void GameOverScene()
+    {
         Application.LoadLevel("Tela Game Over");
     }
 }
